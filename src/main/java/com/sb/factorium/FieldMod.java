f84f262ba3fe19d48fd9112898e39b866ba06701
Samuel Beausoleil <samuel.beausoleil@hotmail.com>
2022-02-12 16:18:00 -0500

    Support nested field mods

--- src/main/java/com/sb/factorium/FieldMod.java
+++ src/main/java/com/sb/factorium/FieldMod.java
@@ -4,19 +4,32 @@ import java.lang.reflect.Field;
 
 /**
  * Implement the Modifier interface by modifying a field directly.
+ *
+ * Supports nested targets by separating each field name with a dot.
+ *
+ * e.g.: (on an Address) "city.nCitizens"
  */
 public class FieldMod implements Modifier {
-    protected final String field;
-    protected final Object value;
+    private final String[] nestedFields;
+    private final Object value;
 
     public FieldMod(String field, Object value) {
-        this.field = field;
         this.value = value;
+
+        nestedFields = field.split("\\.");
     }
 
     @Override
     public void apply(Object target) {
         try {
+            String field = nestedFields[0];
+            if (nestedFields.length > 1) {
+                // Iteratively get the real target
+                target = findRealTarget(target);
+                field = nestedFields[nestedFields.length - 1];
+            }
+
+
             Field toChange = target.getClass().getDeclaredField(field);
             boolean originalAccess = toChange.isAccessible();
             toChange.setAccessible(true);
@@ -25,5 +38,16 @@ public class FieldMod implements Modifier {
         } catch (NoSuchFieldException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private Object findRealTarget(Object target) throws NoSuchFieldException, IllegalAccessException {
+        for (int i = 0; i < nestedFields.length - 1; i++) {
+            Field targetField = target.getClass().getDeclaredField(nestedFields[i]);
+            boolean originalAccess = targetField.isAccessible();
+            targetField.setAccessible(true);
+            target = targetField.get(target);
+            targetField.setAccessible(originalAccess);
+        }
+        return target;
     }
 }
